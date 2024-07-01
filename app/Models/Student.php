c85e77f096679c08044925d0359181cf7c71c2ea
RonnelG <ronnel.gagol12@gmail.com>
2024-06-30 18:53:08 -0800

    Add files via upload
    Gagol BSIT - 3B

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;use HasFactory;
+
+    protected $fillable = [
+        'firstname', 'lastname', 'birthdate', 'sex', 'address', 'year', 'course', 'section'
+    ];
+
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class);
+    }
+}
